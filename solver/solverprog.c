1d87cb36c9c55af224ac3a88a31a6447365f8119
Lorenzo Taalba <lorenzo.taalba@epita.fr>
2022-10-03 14:16:24 +0200

    skeleton

---
+++ solver/solverprog.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//_____________________________initialisation___________________________________
+
+void fill(char s[81][10])
+{
+    //for each empty box, init possible numbers
+    for(size_t i = 0;i<81;i++)
+        if(s[i][0] == (char)0)
+            for(short j = 0;j<10;j++)
+                s[i][j] = j;
+}
+
+void del_h(char s[81][10],size_t y)
+{
+    //delete impossible numbers for the line "y"
+    char notpermit[9] = {1,2,3,4,5,6,7,8,9};
+    //find every impossible numbers
+    for(size_t i = y;i<y+9;i++)
+        if(s[i][0] != 0)
+            notpermit[s[i][0]-1] = 0;
+    //delete for the empty box
+    for(size_t i = y;i<y+9;i++)
+        if(s[i][0] == 0)
+            for(size_t j = 1;j<10;j++)
+                s[i][j] = notpermit[j-1];
+}
+
+void del_v(char s[81][10],size_t x)
+{  
+    //delete impossible numbers for the row "x"
+    char notpermit[9] = {0};
+    //find every impossible numbers
+    for(size_t i = x;i<=81;i+=9)
+        if(s[i][0] != 0)
+            notpermit[s[i][0]-1] = s[i][0];
+    //delete for the empty box
+    for(size_t i = x;i<=81;i+=9)
+        if(s[i][0] == 0)
+            for(size_t j = 1;j<10;j++)
+                if(s[i][j] == notpermit[j-1])
+                    s[i][j] = 0;
+}
+
+void del_s(char s[81][10],size_t p)
+{
+    //delete impossible numbers for the 3x3 square at "p"
+    char notpermit[9] = {0};
+    //find every impossible numbers
+    for(size_t i = 0;i<3;i++)
+        for(size_t j=0;j<3;j++)
+            if(s[p + i*9 +j][0] != 0)
+                notpermit[s[p+i*9+j][0]-1] = s[p+i*9+j][0];
+    //delete for the empty box
+    for(size_t i = 0;i<3;i++)
+        for(size_t j=0;j<3;j++)
+            if(s[p+i*9+j][0] == 0)
+                for(size_t k = 1;k<10;k++)
+                    if(s[p+i*9+j][k] == notpermit[k-1])
+                        s[p+i*9+j][k] = 0;
+}
+
+void one(char s[81][10])
+{
+    //init every empty box with only one possible number
+    for(size_t i = 0;i<81;i++)
+        if(s[i][0] == 0)
+        {
+            char nbr = 0;
+            size_t j = 1;
+            while(j < 10 && !(s[i][j] && nbr))
+            {
+                if(s[i][j])
+                    nbr = s[i][j];
+                j++;
+            }
+            if(j == 10)
+                s[i][0] = nbr;
+        }
+            
+}
+
+void init(char s[81][10])
+{
+    //init sudoku board
+    fill(s);
+    for(size_t i = 0;i<81;i+=9)
+        del_h(s,i);
+    for(size_t i = 0;i<9;i++)
+        del_v(s,i);
+    for(size_t i = 0;i<81;i+=27)
+        for(size_t j = 0;j<9;j+=3)
+            del_s(s,i+j);
+    one(s);
+}
+
+//___________________________________check______________________________________
+
+char valid_h(char s[81][10], size_t y,char n)
+{
+    //check if the line is valid
+    size_t i = 0;
+    while(i < 9 && s[y*9+i][0] != n)
+        i++;
+    return i == 9;
+}
+
+char valid_v(char s[81][10], size_t x,char n)
+{
+    //check if the row is valid
+    size_t i = 0;
+    while(i < 81 && s[x+i][0] != n)
+        i+=9;
+    return i >= 81;
+}
+
+char valid_s(char s[81][10], size_t p,char n)
+{
+    //check if the 3x3 square is valid
+    for(size_t i = 0;i<3;i++)
+        for(size_t j=0;j<3;j++)
+            if(s[p + i*9 +j][0] == n)
+                return 0;
+    return 1;
+}
+
+char valid(char s[81][10], size_t i, char n)
+{
+    return valid_h(s,y_of(i),n) && valid_v(s,x_of(i),n) && valid_s(s,i_of(i),n);
+}
+
+//________________________________solver________________________________________
+
+char force(char s[81][10], size_t i)
+{
+    //the board is complete
+    if(i >= 81)
+        return 1;
+    //if the box is not empty go to the next
+    if(s[i][0])
+        return force(s,i+1);
+    //try every possible numbers
+    size_t j = 0;
+    j = valid_numb(s,i,j);
+    while(j < 10)
+    {
+        //if the number is valid go to the next
+        if(valid(s,i,s[i][j]))
+        {
+            s[i][0] = s[i][j];
+            if(!force(s,i+1))
+                s[i][0] = 0;
+            else
+                return 1;
+        }
+        j = valid_numb(s,i,j);
+    }
+    return 0;
+}
+
+//______________________________________________________________________________
---
+++ solver/tools.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+
+//______________________________________________________________________________
+
+size_t space[18] = {3,7,15,19,27,31,40,44,52,56,64,68,77,81,89,93,101,105};
+size_t back[11] = {11,23,35,36,48,60,72,73,85,97,109};
+
+//______________________________________________________________________________
+ 
+size_t x_of(size_t i)
+{
+    return i % 9;
+}
+ 
+size_t y_of(size_t i)
+{
+    return i / 9;
+}
+
+size_t i_of(size_t i)
+{
+    return (y_of(i) / 3)*27 + (x_of(i) / 3)*3;
+}
+
+//______________________________________________________________________________
+
+size_t valid_numb(char s[81][10], size_t i, size_t j)
+{
+    //return the next possible number
+    j++;
+    while(s[i][j] == 0)
+        j++;
+    return j;
+}
+
+//______________________________________________________________________________
+
+void print(char s[81][10])
+{
+    for(size_t i = 0; i < 9; i++)
+    {
+        for(size_t j = 0; j < 9; j++)
+            printf("%hhi ",s[i*9+j][0]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+//______________________________________________________________________________
+
+void write(char* filename,char* result)
+{
+    strcat(filename,".result");
+    FILE* output_file = fopen(filename, "w+");
+    if (!output_file) {
+        errx(1,"fopen");
+    }
+
+    fwrite(result, 1, strlen(result), output_file);
+    printf("Done Writing!\n");
+
+    fclose(output_file);
+}
+
+void read(char* filename,char* buffer, int len)
+{
+    
+    FILE* input_file = fopen(filename , "r+");
+    if (!input_file) {
+        errx(1,"fopen");
+    }
+    
+    fread(buffer, len+1, 1, input_file);
+    fclose(input_file);
+}
+
+void verified(char *board,char *output)
+{
+    size_t j = 0 ; size_t ispace = 0 ; size_t iback = 0;
+    for(size_t i = 0; i < 110; i++)
+    {
+        if(i == space[ispace] )
+            if(board[i] != ' ')
+                errx(2,"invalid sudoku file: space missing");
+                
+        else if(i == back[iback] )
+            if(board[i] != '\n')
+                errx(2,"invalid sudoku file: back line missing");
+                
+        else if(board[i] == ' ' || board[i] ==  '\n')
+            errx(2,"invalid sudoku file: invalid numbers");
+        else
+        {
+            output[j] = board[i];
+            j++;
+        }
+    }
+    output[j] = 0;
+}
+
+void translate(char* board,char result[81][10])
+{
+    char vboard[82];
+    verified(board,vboard);
+    for(size_t i = 0;i<81;i++)
+        if(vboard[i] != '.')
+            result[i][0] = vboard[i]-48;
+}
+
