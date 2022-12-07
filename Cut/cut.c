fd5b8048ddc54ecb5a793d90e6d39a17805c78d8
sacha.reggiani <sacha.reggiani@epita.fr>
2022-12-07 12:54:53 +0100

    same

--- Cut/cut.c
+++ Cut/cut.c
@@ -16,58 +16,107 @@ void cut(SDL_Surface* surface, SDL_Surface*** L)
 
 
     int length = (surface->w)/9;
+    //length *= 0.95;
+    int pas = length*0.10;
+    int spas = length*0.05;
     //SDL_Surface* L[81] = {};
     size_t count = 0;
 
     int nb_pixel = surface->w;  
+    size_t i2 = 0;
+    size_t j2 = 0;
+    //i2 += pas;
+    //j2 += pas;
 
-    for(size_t i = 0; i < (size_t)nb_pixel-1; i+=length) 
+    for(size_t i = 0; i < 9; i++)
+    {
+	    for(size_t j = 0; j < 9; j++)
+	    {
+		    SDL_Surface* surface_tmp = SDL_CreateRGBSurfaceWithFormat(0, surface->w, surface->h, 28, format->format);
+		    SDL_Rect rect;
+		    rect.x = j2;
+		    rect.y = i2;
+		    rect.w = length;//nb_pixel;
+		    rect.h = length;//nb_pixel;
+
+		    /*if(i%3 == 0)
+			    rect.w -= pas;
+		    else
+			    rect.w -= spas;
+		    if(j%3 == 0)
+			    rect.h -= pas;
+		    else
+			    rect.h -= spas;*/
+					  
+		    if(!SDL_BlitSurface(surface, &rect, surface_tmp, NULL)) // blitsSurface
+			printf("error");
+
+		    (*L)[count] = surface_tmp;
+		    count++;
+
+		    j2 += length;
+
+	    }
+	    //j2 = pas;
+	    j2 = 0;
+	    i2 += length;
+    }
+
+    /*for(size_t i = 0; i < (size_t)nb_pixel-1; i+=length) 
     {
 	    for(size_t j = 0; j < (size_t)nb_pixel-1; j+=length)
 	    {
-		    //SDL_Surface* surface_tmp = (SDL_Surface*)malloc(sizeof(SDL_CreateRGBSurfaceWithFormat(0, surface->w, surface->h, 28, format->format)));//peut etre 32
 		    SDL_Surface* surface_tmp = SDL_CreateRGBSurfaceWithFormat(0, surface->w, surface->h, 28, format->format);
-		    //surface_tmp = malloc(sizeof(SDL_Surface));
-		    //Uint32* pixels_tmp = surface_tmp->pixels;
-		    //SDL_LockSurface(surface_tmp);
-		    //size_t pixels_tmp_count = 0;
-		    
 		    SDL_Rect rect;
 		    rect.x = j;
 		    rect.y = i;
 		    rect.w = length;//nb_pixel;
 		    rect.h = length;//nb_pixel;
+		    
+		    if(!SDL_BlitSurface(surface, &rect, surface_tmp, NULL))
+			printf("error");
 
-		    /*SDL_Rect dest;
-		    dest.x = 0;
-		    dest.y = 0;
-		    dest.w = nb_pixel;
-		    dest.h = nb_pixel;*/
+		    (*L)[count] = surface_tmp;
+		    count++;
+
+
+
+
+		    //SDL_Surface* surface_tmp = (SDL_Surface*)malloc(sizeof(SDL_CreateRGBSurfaceWithFormat(0, surface->w, surface->h, 28, format->format)));//peut etre 32
+		    //surface_tmp = malloc(sizeof(SDL_Surface));
+		    //Uint32* pixels_tmp = surface_tmp->pixels;
+		    //SDL_LockSurface(surface_tmp);
+		    //size_t pixels_tmp_count = 0;
+		    
+
+		    //SDL_Rect dest;
+		    //dest.x = 0;
+		    //dest.y = 0;
+		    //dest.w = nb_pixel;
+		    //dest.h = nb_pixel;
 
 		    //SDL_BlitSurface(surface, &rect, surface_tmp, NULL);
 		    
 		    //SDL_BlitScaled(surface, &rect, surface_tmp, NULL);
 
 
-		    if(!SDL_BlitSurface(surface, &rect, surface_tmp, NULL))
-			printf("error");
 
 		    
 		    //SDL_UnlockSurface(surface_tmp);
 		    //printf("%zi\n",count);
-		    //*L = malloc(sizeof(SDL_Surface) * 82);
+		    *L = malloc(sizeof(SDL_Surface) * 82);
 		    //if(*L == NULL)
 		    //	    return;
 		    //(*L)[count] = malloc(sizeof(SDL_Surface));
-		    (*L)[count] = surface_tmp;
-		    //*(*L + count) = surface_tmp;
+		    *(*L + count) = surface_tmp;
 		    //L[count] = surface_tmp;
-		    count++;
 
 
 
+		    //j += length*0.05;
 		}
-	}
+	    //i += length*0.05;
+	}*/
 
 		    
 
---
+++ Cut/main.c
@@ -0,0 +1,132 @@
+#include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "cut.h"
+
+SDL_Surface* load_image(const char* path)
+{
+    SDL_Surface* tmp = IMG_Load(path);
+    SDL_Surface* surface = SDL_ConvertSurfaceFormat(tmp, SDL_PIXELFORMAT_RGB888, 0);
+    SDL_FreeSurface(tmp);
+    return surface;
+}
+
+int main(int argc, char **argv)
+{
+
+    // Checks the number of arguments.
+    if (argc != 2)
+        errx(EXIT_FAILURE, "Usage: image-file");
+
+    // - Initialize the SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // - Create a window.
+    SDL_Window* window = SDL_CreateWindow("Display Image", 0, 0, 0, 0,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+    // - Creates a renderer.
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0); //SDL_RENDERER_ACCELERATED
+    if(renderer == NULL) 
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    // - Create a surface from the colored image.
+    SDL_Surface* s = load_image(argv[1]);
+
+    
+    //SDL_Surface** L = calloc(81, sizeof(SDL_Surface));
+    //SDL_Surface* L[81];
+    SDL_Surface **L = malloc(sizeof(SDL_Surface*) * 81);
+    cut(s, &L);
+
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = 111;
+    rect.h = 111;
+
+    SDL_Rect rect2;
+    rect2.x = 777;
+    rect2.y = 888;
+    rect2.w = 111;
+    rect2.h = 111;
+
+    SDL_Surface* p[2];
+
+    SDL_Surface* tmp = SDL_CreateRGBSurfaceWithFormat(0, s->w, s->h, 28, s->format->format);
+    SDL_Surface* tmp2 = SDL_CreateRGBSurfaceWithFormat(0, s->w, s->h, 28, s->format->format);
+
+
+    SDL_BlitSurface(s, &rect, tmp, NULL);
+    SDL_BlitSurface(s, &rect2, tmp2, NULL);
+
+
+    p[0] = tmp;
+    p[1] = tmp2;
+    
+    printf("oui");
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, *(L+1));
+
+    printf("ok");
+
+    int w, h;
+    if(SDL_QueryTexture(texture, NULL, NULL, &w, &h) != 0)
+	    errx(EXIT_FAILURE, "%s", SDL_GetError());
+		    
+    // - Resize the window according to the size of the image.	   
+    SDL_SetWindowSize(window, w, h);
+
+
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    /*int i = 0;
+    while(L[i] != 0)
+    {
+	    
+    }*/
+
+       
+    // - Convert the surface into grayscale.
+    //surface_to_grayscale(s);
+
+    // - Create a new texture from the grayscale surface.
+    //SDL_Texture* grayT = SDL_CreateTextureFromSurface(renderer, s)
+    
+    SDL_Event event;
+
+
+    while(1)
+    {
+	SDL_WaitEvent(&event);
+	if(event.type == SDL_QUIT)
+		break;
+    }
+
+    // - Free the surface.
+    SDL_FreeSurface(s);
+    free(L);
+    size_t k = 0;
+    /*while(L[k] != 0)
+    {
+	    SDL_FreeSurface(L[k]);
+    }*/
+
+    // - Dispatch the events.
+    //event_loop(renderer, grayT);
+
+    // - Destroy the objects.
+    //SDL_DestroyTexture(grayT);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+
+    
+    return EXIT_SUCCESS;
+}
+
