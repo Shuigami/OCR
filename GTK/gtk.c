72468eb40b3377a87ff59b1fbae1147f663e5d4b
Lorenzo Taalba <lorenzo.taalba@epita.fr>
2022-12-09 19:58:46 +0100

    je rentre chez moi chialer

--- GTK/gtk.c
+++ GTK/gtk.c
@@ -12,7 +12,7 @@ typedef struct UserInterface
     GtkWindow* window;
     GtkDrawingArea* result;
     GtkButton* start_button;
-    GtkButton* choose_button;
+    GtkFileChooserButton* choose_button;
     gint size;
 } UserInterface;
 
@@ -40,6 +40,8 @@ size_t back[11] = {11,23,35,36,48,60,72,73,85,97,109};
 
 void f_read(gchar* filename,char* buffer)
 {
+    g_print("\n%s\n",filename);
+
     FILE* input_file = fopen(filename , "r+");
     if (!input_file) {
         errx(1,"fopen");
@@ -95,11 +97,12 @@ void translate(char* board,gchar *result)
 
 //__________________________________FUNC________________________________________
 
-void update_sdk(Sudoku *sdk,char *file)
+void update_sdk(Sudoku *sdk,gchar *file)
 {
   char *buffer1 = calloc(111,sizeof(char));
   char *buffer2 = calloc(111,sizeof(char));
 
+  g_print("%s",file);
   f_read(file,buffer1);
   file = realloc(file,(strlen(file)+8) * sizeof(char));
   strcat(file,".result");
@@ -175,46 +178,25 @@ gboolean on_configure(GtkWidget *widget, GdkEvent *event, gpointer user_data)
   else
       ui->size = h;
 
+  g_print(" %i ",ui->size);
+
   draw(ui);
   return TRUE;
 }
 
-gboolean on_choose_file(GtkWidget *widget, GdkEvent *event, gpointer user_data)
+gboolean on_choose_file(GtkWidget *widget, gpointer user_data)
 {
     APK *master = user_data;
-    UserInterface *ui = &master->UI;
 
-    g_print("%i",GTK_IS_WINDOW(ui->window));
-  
-    GtkWidget* dialog = gtk_file_chooser_dialog_new ("Open File",
-            master->UI.window,  GTK_FILE_CHOOSER_ACTION_OPEN,("Cancel"),
-            GTK_RESPONSE_CANCEL,("Open"),GTK_RESPONSE_ACCEPT,
-            NULL);
+    master->File = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(master->UI.choose_button));
 
-    gtk_window_set_destroy_with_parent(master->UI.window,TRUE);
-
-    gint res = gtk_dialog_run (GTK_DIALOG (dialog));
-    gtk_widget_destroy (dialog);
-
-    if (res == GTK_RESPONSE_ACCEPT)
-    {
-        GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-        master->File = gtk_file_chooser_get_filename (chooser);
-        //gtk_widget_set_sensitive((GtkWidget *)start_button,TRUE);
-        return TRUE;
-    }
-    return FALSE;
+    g_print("\n%s\n",master->File);
+    return TRUE;
 }
 
-gboolean on_start(GtkWidget *widget, GdkEvent *event,gpointer user_data)
+gboolean start(APK *master)
 {
-  APK *master = user_data;
-
-
-  g_print("%i",GTK_IS_WINDOW(master->UI.window));
-  
-
-
+  if(master->File)
   {
     //calcul sudoku
     master->SDK.solved = TRUE;
@@ -226,22 +208,27 @@ gboolean on_start(GtkWidget *widget, GdkEvent *event,gpointer user_data)
   return FALSE;
 }
 
+
+gboolean on_start(GtkWidget *widget, GdkEvent *event,gpointer user_data)
+{
+  APK *master = user_data;
+  return start(master);
+}
+
 // Event handler for the "key-release-event" signal.
 gboolean on_key_release(GtkWidget *widget, GdkEvent *event, gpointer user_data)
 {
   APK* master = user_data;
-
-  g_print("%i",GTK_IS_WINDOW(master->UI.window));
-  
+  g_print("\n%s\n",master->File);
 
   // If the 'f' key is released, stops the player 1.
-  if ( ((GdkEventKey *)event)->keyval == GDK_KEY_3270_Enter)
+  if ( ((GdkEventKey *)event)->keyval == GDK_KEY_s)
   {
     return on_start(widget,event,master);
   }
-  else if ( ((GdkEventKey *)event)->keyval == GDK_KEY_Tab)
+  else if ( ((GdkEventKey *)event)->keyval == GDK_KEY_c)
   {
-    return on_choose_file(widget,event,master);
+    return on_choose_file(widget,master);
   }
   return TRUE;
 }
@@ -268,20 +255,20 @@ int main()
 
 
   // Gets the widgets.
-  GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.ocr"));
+  GtkWindow* window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
   GtkDrawingArea* result = GTK_DRAWING_AREA(gtk_builder_get_object(builder, "result"));
   GtkButton* start_button = GTK_BUTTON(gtk_builder_get_object(builder, "start_button"));
-  GtkButton* choose_button = GTK_BUTTON(gtk_builder_get_object(builder, "choose_button"));
+  GtkFileChooserButton* choose_button = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "choose_button"));
 
-
+  gtk_widget_show_all(GTK_WIDGET(window));
 
   UserInterface ui =
   {
-	  .window = window,
-	  .result = result,
-	  .start_button = start_button,
-	  .choose_button = choose_button,
-	  .size = 0,
+      .window = window,
+      .result = result,
+      .start_button = start_button,
+      .choose_button = choose_button,
+      .size = 0,
   };
 
 
@@ -304,7 +291,7 @@ int main()
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(result, "draw", G_CALLBACK(on_draw), &master);
   g_signal_connect(start_button, "clicked", G_CALLBACK(on_start), &master);
-  g_signal_connect(choose_button, "clicked", G_CALLBACK(on_choose_file), &master);
+  g_signal_connect(choose_button, "file-set", G_CALLBACK(on_choose_file), &master);
   g_signal_connect(window, "key_release_event", G_CALLBACK(on_key_release), &master);
   g_signal_connect(result,"configure-event",G_CALLBACK(on_configure),&ui);
 
